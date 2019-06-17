1815f6d0e5e05d28f43472f74915f7a11cbbf2b5
big3_l <big3_l@hotmail.com>
2019-06-17 16:02:35 +0200

    ,

--- JavaScript/Loop/for-loop.js
+++ JavaScript/Loop/for-loop.js
@@ -1,14 +1,17 @@
 // print numbers between 1 and 100 + 9
+
 for (let i = 1; i <= 100; i++) {
 
     console.log(i + " + 9 = " + i * 9);
 }
+
 // the Grade Assigner
 // if the result > 89 then you get A
 // if the result > 80 then you get B
 // if the result > 70 then you get C
 // if the result > 65 then you get D
 // if the result < 65 then you get F
+
 for (let i = 100; i >= 1; i--) {
 
     if (i > 89) {
@@ -27,19 +30,86 @@ for (let i = 100; i >= 1; i--) {
         console.log(i + " you get F")
     }
 }
+
 ///////////////////////////////////////////////
 // print 100 stars, 10 stars in each row
+
 for (let i = 1; i <= 100; i++) {
     document.write(' * ');
     if (i % 10 == 0) {
         document.write('<br>');
     }
 }
-//////////////////////////////
+
+/////////////////////////////////////////////////
+document.write('<br>');
+
 for (i = 0; i < 10; i++) {
 
     for (let c = 0; c < 10; c++) {
         document.write(' + ');
     }
+    document.write('<br>');
+}
+
+/////////////////////////////////////////////////
+//Print triangle of start from 4 lines and 4 columns
+document.write('<br>');
+
+for (let i = 0; i < 10; i++) {
+
+    for (let j = 0; j <= i; j++) {
+        document.write(' * ');
+    }
+    document.write('<br>');
+}
+
+/////////////////////////////////////////////////
+document.write('<br>');
+
+for (j = 0; j < 10; j++) {
+
+    for (let k = j; k < 10; k++) {
+        document.write(' § ');
+    }
+    document.write('<br>');
+}
+
+/////////////////////////////////////////////////
+document.write('<br>');
+
+for (j = 0; j < 10; j++) {
+
+    for (let k = 0; k <= j; k++) {
+        document.write(' § ');
+    }
+    document.write('<br>');
+}
+////////////////////////////////////////////////
+document.write('<br>');
+
+for (let i = 1; i <= 8; i++) {
+
+    for (k = 1; k <= (8 - i); k++) {
+        document.write('&nbsp');
+    }
+    for (let j = 1; j < i; j++) {
+        document.write('* ');
+    }
+    document.write('<br>');
+}
+
+////////////////////////////////////////////////////////
+for (let i = 0; i <= 4; i++) {
+
+    for (let j = 0; j < 4 - i; j++) {
+        alert('j=' + j)
+        document.write('&nbsp;');
+    }
+    for (let c = 0; c < i; c++) {
+        document.write('* ');
+        alert('c=' + c)
+    }
+    alert('i=' + i)
     document.write('<br>');
 }
