3c659f0786d92b471bbc7d3b5d32b1deb0e5a4f7
big3_l <big3_l@hotmail.com>
2019-07-10 16:21:08 +0200

    Upload validator, completed todo list

--- JavaScript/DOM /ToDoList/todo.js
+++ JavaScript/DOM /ToDoList/todo.js
@@ -1,24 +1,54 @@
 /////////////////////////////////////
 //TO DO LIST
+//Adding items to the to do list
+
+let mainDiv = document.getElementById("mainDiv");
+let input = document.getElementById("textInput");
 
 function newList() {
-    let newItem = document.createElement("h3");
-    let input = document.getElementById("textInput");
-    let newItemName = document.createTextNode(input.value);
+    if (input.value !== "") {
+        let newItem = document.createElement("h3");
+        let newItemName = document.createTextNode(input.value);
 
-    newItem.appendChild(newItemName);
-    document.getElementById("mainDiv").appendChild(newItem);
-    input.value = "";
+        newItem.appendChild(newItemName);
+        document.getElementById("mainDiv").appendChild(newItem);
+        input.value = "";
+        input.focus();
+
+        //Remove items from list
+        let el = document.getElementsByTagName("h3");
+        for (let i = 0; i < el.length; i++) {
+            el[i].onclick = function () {
+                mainDiv.removeChild(this);
+            }
+        }
+    }
+    else {
+        alert("Enter an item to the to do list");
+    }
 }
-document.getElementById("button").addEventListener("click", function () {
-    newList();
-});
+function pressEnter(myEvent) {
+    if (myEvent.keyCode == 13) {
+        newList();
+    }
+}
+button.addEventListener("click", newList);
+input.addEventListener("keypress", pressEnter);
 
-let newItem = document.createElement("h3");
-newItem.addEventListener("click", function () {
-    
-    newItem.removechild(newItem.childNodes[0])
-});
+
+//Another attempt at removing in
+// let newItem = document.createElement("h3");
+
+// newItem.addEventListener("click", function () {
+//     newItem.removechild(newItem.childNodes[0])
+// });
+
+
+//Other Attempts
+// document.getElementById("mainDiv").addEventListener("click", function () {
+//     mainDiv.removeChild(mainDiv.childNodes[0]);
+// });
+
 
 //Ahmads way...
 // document.getElementById('add').addEventListener('click', function () {
