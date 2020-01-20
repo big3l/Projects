1429bd59feee044246246f7e1c63f29af1b576ff
big3_l <big3_l@hotmail.com>
2020-01-20 16:16:12 +0100

    login errors

--- Node.js/12 - Express/Data Server/15-passport-login-app/app.js
+++ Node.js/12 - Express/Data Server/15-passport-login-app/app.js
@@ -8,9 +8,9 @@ const ejs = require("ejs");
 const colors = require("colors");
 const app = express();
 
-const expressLayouts = require ("express-ejs-layouts")
+const expressLayouts = require("express-ejs-layouts");
 app.use(expressLayouts);
-app.set('view engine', 'ejs');
+app.set("view engine", "ejs");
 
 const dotenv = require("dotenv");
 dotenv.config({ path: "./config/config.env" });
@@ -18,38 +18,42 @@ dotenv.config({ path: "./config/config.env" });
 const connectDB = require("./config/db");
 connectDB();
 
-
 // Body parser for Posting Data
 app.use(express.urlencoded({ extended: true }));
 
+// Connect Flash
+app.use(flash());
+
 // Express session
-app.use(session({
-    secret : 'secret',
+app.use(
+  session({
+    secret: "secret",
     resave: true,
-    saveUninitialized : true
-}));
+    saveUninitialized: true
+  })
+);
 
 // Passport Middleware
 app.use(passport.initialize());
 app.use(passport.session());
 
-// Connect Flash
-app.use(flash());
-
 // Global variables
-app.use((req,res,next)=> {
-    res.locals.success_msg = req.flash('success_msg');
-    res.locals.error_message = req.flash('error_msg');
-    res.locals.error = req.flash('error');
-    next();
+app.use((req, res, next) => {
+  res.locals.success_msg = req.flash("success_msg");
+  res.locals.error_message = req.flash("error_msg");
+  // res.locals.error = req.flash("error");
+  next();
 });
 
-// Routes 
-app.use('/' , require('./routes/index'));
-app.use('/users' ,require('./routes/users'));
+// Routes
+app.use("/", require("./routes/index"));
+app.use("/users", require("./routes/users"));
 
 const PORT = process.env.PORT || 5005;
-const server = app.listen(PORT, console.log("Server started on port 5005".rainbow));
+const server = app.listen(
+  PORT,
+  console.log("Server started on port 5005".rainbow)
+);
 console.log(
   `Server Started on Port ${PORT} in ${process.env.NODE_ENV} mode`.magenta
 );
--- Node.js/12 - Express/Data Server/15-passport-login-app/routes/users.js
+++ Node.js/12 - Express/Data Server/15-passport-login-app/routes/users.js
@@ -1,14 +1,75 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
+const bcrypt = require("bcryptjs");
+
+// import User model
+const User = require("../models/User");
 
 // Login Page
-router.get('/login', (req,res)=>{
-    res.render('login')
+router.get("/login", (req, res) => {
+  res.render("login");
 });
 
-// Register Page
-router.get('/register', (req,res)=>{
-    res.render('register')
-})
+// Registration Page
+router.get("/register", (req, res) => {
+  res.render("register");
+});
+
+// Handle Registration
+router.post("/register", (req, res) => {
+  // console.log(req.body);
+  // res.send('Hello from the other side')
+  const { name, email, password, password2 } = req.body;
+  let error = [];
+  // checkin the required fields
+  if (!name || !email || !password || !password2) {
+    error.push({ msg: "Please complete all fields" });
+  }
+  // check passwords match
+  if (password !== password2) {
+    error.push({ msg: "Passwords do not match" });
+  }
+  // check password length
+  if (password.length < 6) {
+    error.push({ msg: "password must be at least 6 charachters long" });
+  }
+  if (error.length > 0) {
+    res.render("register", {
+      error,
+      name,
+      email,
+      password,
+      password2
+    });
+  } else {
+    // validation passed
+    User.findOne({ email: email }).then(data => {
+      if (data) {
+        // we found the email in our database
+        error.push({ msg: "Email is already registered" });
+        res.render("register", { error, name, email, password, password2 });
+      } else {
+        const newUser = new User({ name, email, password });
+        // Hash password
+        bcrypt.genSalt(10, (err, salt) => {
+          bcrypt.hash(newUser.password, salt, (err, hash) => {
+            if (err) throw err;
+            newUser.password = hash;
+            console.log("the hashed password is : " + newUser.password);
+            newUser
+              .save() //saved user in database
+              .then(user => {
+                req.flash('success_msg', 'You are registered, please login.')
+                res.redirect("/users/login");
+              })
+              .catch(err => {
+                console.log(err);
+              });
+          });
+        });
+      }
+    });
+  }
+});
 
 module.exports = router;
